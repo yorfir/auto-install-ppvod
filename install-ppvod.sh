694be13002fdd5444e477de248592716eca4cf3a
Yorfir <yorfir@users.noreply.github.com>
2017-04-29 23:04:11 +0800

    Update install-ppvod.sh

--- install-ppvod.sh
+++ install-ppvod.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+# Version:0.1
+# Author:www.bgp.la
+mkdir /yunzhuanma
+cd /yuanzhuanma
+wget --no-check-certificate https://raw.githubusercontent.com/yorfir/auto-install-ppvod/master/version/yunzhuanma-2.11.6-linux.tar.gz
+tar zxvf yunzhuanma-2.11.6-linux.tar.gz
+cd bin
+./start.sh
+echo -e "\033[37;31;5m云转码启动成功...\033[39;49;0m"
