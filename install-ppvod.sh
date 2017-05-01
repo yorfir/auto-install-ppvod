f405f9c9a7dd3e297843c9ae66d5b49be5b92858
Yorfir <yorfir@users.noreply.github.com>
2017-05-01 16:48:22 +0800

    Update install-ppvod.sh

--- install-ppvod.sh
+++ install-ppvod.sh
@@ -2,7 +2,6 @@
 # Version:0.1
 # Author:www.bgp.la
 mkdir /yunzhuanma
-cd /yuanzhuanma
 wget --no-check-certificate https://raw.githubusercontent.com/yorfir/auto-install-ppvod/master/version/yunzhuanma-2.13.8-linux.tar.gz
 tar zxvf yunzhuanma-2.11.6-linux.tar.gz /yunzhuanma
 cd /yunzhuanma/bin
