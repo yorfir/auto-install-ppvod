fe30d3561ae59567505628d0cb040d9a8f380b56
Yorfir <yorfir@users.noreply.github.com>
2017-05-01 16:46:58 +0800

    Update install-ppvod.sh

--- install-ppvod.sh
+++ install-ppvod.sh
@@ -4,8 +4,8 @@
 mkdir /yunzhuanma
 cd /yuanzhuanma
 wget --no-check-certificate https://raw.githubusercontent.com/yorfir/auto-install-ppvod/master/version/yunzhuanma-2.13.8-linux.tar.gz
-tar zxvf yunzhuanma-2.11.6-linux.tar.gz
-cd bin
+tar zxvf yunzhuanma-2.11.6-linux.tar.gz /yunzhuanma
+cd /yunzhuanma/bin
 ./start.sh
 echo "/yunzhuanma/bin/start.sh" >> /etc/rc.local
 echo -e "\033[37;31;5m云转码安装了成功，并已设置开机自动启动...\033[39;49;0m"
