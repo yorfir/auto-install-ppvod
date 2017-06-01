dcd39bd2ac991655ab99abafd4a301263988408d
Yorfir <yorfir@users.noreply.github.com>
2017-06-01 20:49:00 +0800

    Update install-ppvod.sh

--- install-ppvod.sh
+++ install-ppvod.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Version:0.1
 # Author:www.bgp.la
-mkdir /yunzhuanma
-wget --no-check-certificate -P /yunzhuanma https://raw.githubusercontent.com/yorfir/auto-install-ppvod/master/version/yunzhuanma-last.tar.gz
-cd /yunzhuanma && tar -zxvf yunzhuanma-last.tar.gz -C /yunzhuanma
-cd /yunzhuanma/bin && ./start.sh
-echo "/yunzhuanma/bin/start.sh" >> /etc/rc.local
+mkdir /home/yunzhuanma
+mkdir /home/videos
+wget --no-check-certificate -P /home/yunzhuanma https://raw.githubusercontent.com/yorfir/auto-install-ppvod/master/version/yunzhuanma-last.tar.gz
+cd /home/yunzhuanma && tar -zxvf yunzhuanma-last.tar.gz -C /home/yunzhuanma
+cd /home/yunzhuanma/bin && ./start.sh
+echo "/home/yunzhuanma/bin/start.sh" >> /etc/rc.local
 echo -e "\033[37;31;5m云转码安装了成功，并已设置开机自动启动...\033[39;49;0m"
